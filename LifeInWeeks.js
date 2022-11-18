aaa172a8e13e153d510854721c70dc3104da58fb
Carl Kevin Galvan <99018607+CKevinGalvan@users.noreply.github.com>
2022-11-18 15:20:34 -0800

    Create LifeInWeeks.js

---
+++ LifeInWeeks.js
@@ -0,0 +1,6 @@
+function lifeInWeeks(age) {
+    console.log("If you are "+age+" years old\n");
+    console.log("You have "+365*(90-age) +" days, "+52*(90-age)+" weeks, and "+(90-age)*(12)+" months left.");
+}
+
+lifeInWeeks(51);
